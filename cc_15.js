8aa859f34b13ae7f4557c9bd586270f623c8e964
Sophia Dolan <25srdolan@gmail.com>
2025-03-16 16:47:21 -0400

    Task 2 - Adding Risk Items

--- cc_15.js
+++ cc_15.js
@@ -3,4 +3,39 @@
 const riskDashboard = document.getElementById("riskDashboard")
 
 console.log("Risk Dashboard Loaded")
-//logging risk dashboard
+
+//logging risk dashboa
+ 
+//task 2
+
+document.getElementById("riskForm").addEventListener("submit", function(event) {
+    event.preventDefault() //stops page from reloading when it is submitted
+
+    const riskName = document.getElementById("riskName").value
+    const riskLevel = document.getElementById("riskLevel").value
+    const riskDepartment = document.getElementById("riskDepartment").value
+    //gets value from form 
+
+.
+    addRiskItem(riskName, riskLevel, riskDepartment)
+    //created a new risk card
+
+
+    this.reset()
+   }) //resets after submitting 
+
+    
+    function addRiskItem(riskName, riskLevel, riskDepartment) {
+        const riskCard = document.createElement("div") //created a new risk card
+
+        
+        riskCard.setAttribute("class", "risk-card")
+        riskCard.setAttribute("id", riskLevel)
+        //sets class and ID
+
+        
+        riskCard.innerHTML = 
+            `<h3>${riskName}</h3>
+            <p><strong>Level:</strong> ${riskLevel}</p>
+            <p><strong>Department:</strong> ${riskDepartment}</p>`}
+            //added the conetent inside carsd with using html template
